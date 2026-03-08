ab30489a6434452b7d9b621387aec53d15e351cc
Eugenio Perez <eugenio.fernando@icloud.com>
2026-03-08 22:16:05 +0100

    Run prisma db push at app startup to migrate mounted /data DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,5 +15,8 @@ if [ "$#" -gt 0 ]; then
   exec "$@"
 fi
 
+echo "Running prisma db push..."
+prisma db push --skip-generate
+
 echo "Starting gunicorn..."
 exec gunicorn run:app --bind 0.0.0.0:8080 -k gthread --threads 4 -w 1
