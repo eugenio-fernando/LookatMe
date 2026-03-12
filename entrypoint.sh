c8a2ca19c427d6df3d3f274ea25d82616ecbec34
Eugenio Perez <eugenio.fernando@icloud.com>
2026-03-12 08:12:23 +0100

    fix: allow prisma DateTime cast during startup migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ "$#" -gt 0 ]; then
 fi
 
 echo "Running prisma db push..."
-prisma db push --skip-generate
+prisma db push --skip-generate --accept-data-loss
 
 echo "Starting gunicorn..."
 exec gunicorn run:app --bind 0.0.0.0:8080 -k gthread --threads 4 -w 1
